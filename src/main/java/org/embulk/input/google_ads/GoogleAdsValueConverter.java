160e71e9eba42844f6bf819d24b9dec3d96f05dd
giwa <ugw.gi.world@gmail.com>
2020-09-30 13:09:39 +0900

    add micro fields

--- src/main/java/org/embulk/input/google_ads/GoogleAdsValueConverter.java
+++ src/main/java/org/embulk/input/google_ads/GoogleAdsValueConverter.java
@@ -4,7 +4,30 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GoogleAdsValueConverter {
-    private static final List<String> MICRO_FIELDS = Arrays.asList("metrics.cost_micros", "metrics.average_cpc", "metrics.cost_per_all_conversions");
+    private static final List<String> MICRO_FIELDS = Arrays.asList(
+            "metrics.cost_micros",
+            "metrics.average_cpc",
+            "metrics.cost_per_all_conversions",
+            "metrics.active_view_cpm",
+            "metrics.active_view_measurable_cost_micros",
+            "metrics.average_cost",
+            "metrics.average_cpm",
+            "metrics.cost_per_conversion",
+            "metrics.benchmark_average_max_cpc",
+            "ad_group_criterion.effective_cpc_bid_micros",
+            "ad_group_criterion.effective_cpm_bid_micros",
+            "ad_group_criterion.position_estimates.estimated_add_cost_at_first_position_cpc",
+            "ad_group.cpv_bid_micros",
+            "ad_group.effective_target_cpa_micros",
+            "bidding_strategy.target_cpa.cpc_bid_ceiling_micros",
+            "bidding_strategy.target_cpa.cpc_bid_floor_micros",
+            "bidding_strategy.target_roas.cpc_bid_ceiling_micros",
+            "bidding_strategy.target_roas.cpc_bid_floor_micros",
+            "bidding_strategy.target_spend.cpc_bid_ceiling_micros",
+            "bidding_strategy.target_spend.target_spend_micros",
+            "campaign_budget.amount_micros",
+            "campaign_budget.recommended_budget_amount_micros"
+    );
 
     public static boolean shouldApplyMicro(String name){
         return MICRO_FIELDS.contains(name);
--- src/test/java/org/embulk/input/google_ads/TestGoogleAdsValueConverter.java
+++ src/test/java/org/embulk/input/google_ads/TestGoogleAdsValueConverter.java
@@ -10,7 +10,30 @@ public class TestGoogleAdsValueConverter {
 
     @Test
     public void testShouldApplyMicro(){
-        List<String> microFields = Arrays.asList("metrics.cost_micros", "metrics.average_cpc", "metrics.cost_per_all_conversions");
+        List<String> microFields = Arrays.asList(
+                "metrics.cost_micros",
+                "metrics.average_cpc",
+                "metrics.cost_per_all_conversions",
+                "metrics.active_view_cpm",
+                "metrics.active_view_measurable_cost_micros",
+                "metrics.average_cost",
+                "metrics.average_cpm",
+                "metrics.cost_per_conversion",
+                "metrics.benchmark_average_max_cpc",
+                "ad_group_criterion.effective_cpc_bid_micros",
+                "ad_group_criterion.effective_cpm_bid_micros",
+                "ad_group_criterion.position_estimates.estimated_add_cost_at_first_position_cpc",
+                "ad_group.cpv_bid_micros",
+                "ad_group.effective_target_cpa_micros",
+                "bidding_strategy.target_cpa.cpc_bid_ceiling_micros",
+                "bidding_strategy.target_cpa.cpc_bid_floor_micros",
+                "bidding_strategy.target_roas.cpc_bid_ceiling_micros",
+                "bidding_strategy.target_roas.cpc_bid_floor_micros",
+                "bidding_strategy.target_spend.cpc_bid_ceiling_micros",
+                "bidding_strategy.target_spend.target_spend_micros",
+                "campaign_budget.amount_micros",
+                "campaign_budget.recommended_budget_amount_micros"
+        );
         for (String field : microFields){
             Assert.assertTrue(GoogleAdsValueConverter.shouldApplyMicro(field));
         }
