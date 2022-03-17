21b5460c3802139e2ca8f259936b30f780bec6cc
kazuki-yane <yanekazuki@yahoo.co.jp>
2022-03-18 00:49:35 +0900

    upgrade_version_10

--- src/main/java/org/embulk/input/google_ads/GoogleAdsInputPlugin.java
+++ src/main/java/org/embulk/input/google_ads/GoogleAdsInputPlugin.java
@@ -1,7 +1,7 @@
 package org.embulk.input.google_ads;
 
-import com.google.ads.googleads.v9.services.GoogleAdsRow;
-import com.google.ads.googleads.v9.services.GoogleAdsServiceClient;
+import com.google.ads.googleads.v10.services.GoogleAdsRow;
+import com.google.ads.googleads.v10.services.GoogleAdsServiceClient;
 import com.google.common.collect.ImmutableList;
 
 import org.embulk.config.ConfigDiff;
