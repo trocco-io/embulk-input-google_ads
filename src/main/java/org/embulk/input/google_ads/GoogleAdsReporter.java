fba1559a30eac079350cb9322a9f7501d01668ee
chikamura <mchikamura01@gmail.com>
2024-01-07 12:50:25 +0900

    @SuppressWarnings("unchecked")

--- src/main/java/org/embulk/input/google_ads/GoogleAdsReporter.java
+++ src/main/java/org/embulk/input/google_ads/GoogleAdsReporter.java
@@ -121,6 +121,7 @@ public class GoogleAdsReporter
     public String convertEnumType(Descriptors.FieldDescriptor key, Map<Descriptors.FieldDescriptor, Object> fields)
     {
         if (key.isRepeated()) {
+            @SuppressWarnings("unchecked")
             List<Descriptors.GenericDescriptor> enumValues = (List<Descriptors.GenericDescriptor>) fields.get(key);
             ArrayNode arrayNode = mapper.createArrayNode();
             for (Descriptors.GenericDescriptor enumValue : enumValues) {
@@ -139,6 +140,7 @@ public class GoogleAdsReporter
     public String convertNonMessageType(Descriptors.FieldDescriptor key, Map<Descriptors.FieldDescriptor, Object> fields)
     {
         if (key.isRepeated()) {
+            @SuppressWarnings("unchecked")
             List<String> values = (List<String>) fields.get(key);
             ArrayNode arrayNode = mapper.createArrayNode();
             for (String val : values) {
@@ -158,6 +160,7 @@ public class GoogleAdsReporter
     {
         if (key.isRepeated()) {
             ArrayNode result = mapper.createArrayNode();
+            @SuppressWarnings("unchecked")
             List<GeneratedMessageV3> field = (List<GeneratedMessageV3>) fields.get(key);
             try {
                 for (GeneratedMessageV3 msg : field) {
